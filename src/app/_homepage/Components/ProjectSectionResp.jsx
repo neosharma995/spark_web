2943b44e2b3df1392cde23b530cf1927ca91a7ce
raaj73906@gmail.com <neosharma995@gmail.com>
2025-02-03 16:52:57 +0530

    updated css in responsive home page

--- src/app/_homepage/Components/ProjectSectionResp.jsx
+++ src/app/_homepage/Components/ProjectSectionResp.jsx
@@ -6,8 +6,8 @@ import './project.css';
 const ProjectSectionResp = () => {
     const pagesDataApi = useContext(SectorDataContext);
     const mainData = pagesDataApi?.pagesDataApi?.find(page => page.slug === 'home')?.acf?.projects_section;
-    
-   
+
+
     const [activeIndex, setActiveIndex] = useState(0);
 
     return (
@@ -17,36 +17,41 @@ const ProjectSectionResp = () => {
             </div>
 
             <div className="resp_proj_view">
-            <div className="proj_left_section resp_view_proj">
-                {mainData?.projects.map((project, index) => (
-                    <div 
-                        key={index} 
-                        className={`proj_images ${index === activeIndex ? 'active' : ''}`}
-                        onClick={() => setActiveIndex(index)}
-                    >
-                        <img src={project.image} alt={project.name} />
-                    </div>
-                ))}
-            </div>
+                <div className="proj_left_section resp_view_proj">
+                    {mainData?.projects.map((project, index) => (
+                        <div
+                            key={index}
+                            className={`proj_images ${index === activeIndex ? 'active' : ''}`}
+                            onClick={() => setActiveIndex(index)}
+                        >
+                            <img src={project.image} alt={project.name} />
+                        </div>
+                    ))}
+                </div>
 
-            <div className="proj_right_section resp_view_proj">
-                <div className='proj_section'>
-                    {mainData?.projects?.length > 0 && (
-                        <div className="prog_content active">
-                            <div className="r_l_section">
- 
-                            <h4>{mainData.projects[activeIndex]?.name}</h4>
-                            <p>{mainData.projects[activeIndex]?.description}</p>
+                <div className="proj_right_section resp_view_proj">
+                    <div className='proj_section'>
+                        {mainData?.projects?.length > 0 && (
+                            <div className="prog_content active">
+                                <div className="r_l_section">
+
+                                    <h4>{mainData.projects[activeIndex]?.name}</h4>
+                                     
+                                    <p>{mainData.projects[activeIndex]?.description}</p>
+                                </div>
+                              
+                                <div className="r_r_section">
+                                <img src={mainData.projects[activeIndex]?.image} alt='image'/> 
+                                </div>
                             </div>
 
-                           
-                        </div>
-                    )}
+
+                        )}
+                    </div>
                 </div>
             </div>
-            </div>
 
-            
+
         </div>
     );
 }
