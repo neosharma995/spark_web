213763ee254852362388c8be7c3797b77838b4a2
Rakhi Sharma <rakhisharma98056@gmail.com>
2025-10-29 18:19:32 +0530

    hero section new

--- src/app/_homepage/Components/heroNew.jsx
+++ src/app/_homepage/Components/heroNew.jsx
@@ -1,6 +1,8 @@
 "use client";
 import React from "react";
-import HeroParticles from "./heroPartical";
+// import HeroParticles from "./heroPartical";
+
+import HeroParticles from "./heroPartical.jsx";
 
 
 const HeroNew = () => {
